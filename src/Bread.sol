dd32d0dfc9f63d6f0c76c34cc6baf06f0dc589d2
Ron Turetzky <turetzkyron@gmail.com>
2024-04-29 11:38:55 +0300

    fix: bumping pragmas

--- src/Bread.sol
+++ src/Bread.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity ^0.8.20;
 
 // Bread - An ERC20 stablecoin fully collateralized by Gnosis Chain xDAI
 // which earns yield via Gnosis Chain sDAI (aka sexyDAI)
--- src/interfaces/ISXDAI.sol
+++ src/interfaces/ISXDAI.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity ^0.8.20;
 
 interface ISXDAI {
     function deposit(uint256 assets, address receiver) external returns (uint256);
